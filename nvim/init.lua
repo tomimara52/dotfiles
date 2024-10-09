40b847fc418c3cb95e6df9822e3ccf35e5c1a0e8
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-10-09 11:03:21 -0300

    use parenthesis in vimplug

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,22 +1,22 @@
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
-  Plug 'preservim/nerdtree'
-  Plug 'neovim/nvim-lspconfig'
-  Plug 'hrsh7th/cmp-nvim-lsp'
-  Plug 'hrsh7th/cmp-buffer'
-  Plug 'hrsh7th/cmp-path'
-  Plug 'hrsh7th/cmp-cmdline'
-  Plug 'hrsh7th/nvim-cmp'
-  Plug 'hrsh7th/cmp-vsnip'
-  Plug 'hrsh7th/vim-vsnip'
-  Plug 'tiagovla/tokyodark.nvim'
-  Plug 'junegunn/fzf.vim'
-  Plug 'rafamadriz/friendly-snippets'
-  Plug 'nvim-lua/plenary.nvim'
-  Plug 'sindrets/diffview.nvim'
-  Plug 'NeogitOrg/neogit'
-  Plug 'tomimara52/nvim-bufbox'
+  Plug('preservim/nerdtree')
+  Plug('neovim/nvim-lspconfig')
+  Plug('hrsh7th/cmp-nvim-lsp')
+  Plug('hrsh7th/cmp-buffer')
+  Plug('hrsh7th/cmp-path')
+  Plug('hrsh7th/cmp-cmdline')
+  Plug('hrsh7th/nvim-cmp')
+  Plug('hrsh7th/cmp-vsnip')
+  Plug('hrsh7th/vim-vsnip')
+  Plug('tiagovla/tokyodark.nvim')
+  Plug('junegunn/fzf.vim')
+  Plug('rafamadriz/friendly-snippets')
+  Plug('nvim-lua/plenary.nvim')
+  Plug('sindrets/diffview.nvim')
+  Plug('NeogitOrg/neogit')
+  Plug('tomimara52/nvim-bufbox')
 vim.call('plug#end')
 
 vim.cmd [[colorscheme tokyodark]]
