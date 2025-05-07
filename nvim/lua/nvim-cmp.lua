c6365950a9ec2f9cc8848b30e1964045be9914d0
Tomás Maraschio <tomas.maraschio@gmail.com>
2025-05-07 13:50:27 -0300

    add border to documentation window

--- nvim/lua/nvim-cmp.lua
+++ nvim/lua/nvim-cmp.lua
@@ -9,7 +9,7 @@
     },
     window = {
       completion = cmp.config.window.bordered(),
-      --documentation = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
