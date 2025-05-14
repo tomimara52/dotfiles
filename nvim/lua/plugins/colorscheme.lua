1cfd44f240607f4110346f9a2d7446f9fa84bede
Tomás Maraschio <tomas.maraschio@gmail.com>
2025-05-13 23:58:41 -0300

    new themes

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,15 +4,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      --vim.cmd([[colorscheme kanagawa-dragon]])
-    end,
-  },
-  {
-    'miikanissi/modus-themes.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme modus_vivendi]])
+      vim.cmd([[colorscheme kanagawa-wave]])
     end,
   },
 }
