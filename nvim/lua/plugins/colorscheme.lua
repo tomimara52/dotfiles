b55e8f751dd182e3e7a4373d5f39cde0c38a31f8
Tomás Maraschio <tomas.maraschio@gmail.com>
2025-05-18 18:46:33 -0300

    customize kanagawa markdown headings

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,7 +4,21 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd([[colorscheme kanagawa-wave]])
+      require('kanagawa').setup({
+        compile = true,
+        overrides = function(colors)
+          return {
+            ["@markup.heading.1"] = { bold = true, fg = colors.theme.term[2], underdashed = true},
+            ["@markup.heading.2"] = { bold = true, fg = colors.theme.term[3], underdashed = true},
+            ["@markup.heading.3"] = { bold = true, fg = colors.theme.term[4], underdashed = true},
+            ["@markup.heading.4"] = { bold = true, fg = colors.theme.term[5], underdashed = true},
+            ["@markup.heading.5"] = { bold = true, fg = colors.theme.term[6], underdashed = true},
+            ["@markup.heading.6"] = { bold = true, fg = colors.theme.term[7], underdashed = true},
+          }
+        end,
+      })
+
+      vim.cmd([[colorscheme kanagawa-dragon]])
     end,
   },
 }
